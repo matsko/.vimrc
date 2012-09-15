1ceee33b03e7c34e3cdc1c5cf493d3c9cc2083b3
Matias Niemelä <matias@yearofmoo.com>
2012-09-15 17:37:57 -0400

    changed the name if the vimrc file

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 git clone git@github.com:matsko/.vim.git ~/.vim
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/.vimrc ~/.vimrc
 mvim +BundleInstall +qall
