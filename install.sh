f17debfb7a6417b44d1cd54c279a2b10b08afc00
Matias Niemelä <matias@yearofmoo.com>
2013-01-31 07:59:02 -0500

    updated the vimrc install file

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-git clone git@github.com:matsko/.vimrc.git ~/.vim
+git clone git://github.com/matsko/.vimrc.git ~/.vim
 git clone git://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 ln -s ~/.vim/.vimrc ~/.vimrc
 mvim +BundleInstall +qall
